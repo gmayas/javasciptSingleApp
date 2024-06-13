8ee9d66b0a823c4615d22bd3c25b81b582a1f588
Gabriel Maya Sánchez. <isc_gmayas@hotmail.com>
2024-06-12 20:16:13 -0600

    first commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+//import './main.css';
+//import './bootswatch.css';
+import 'bootswatch/dist/darkly/bootstrap.min.css';
+
+console.log("hola ...")
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const { Template } = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html'
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+}; 
