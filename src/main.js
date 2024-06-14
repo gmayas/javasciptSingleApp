dbfdc6c87f4effa64a8dceea96f9b5e2862e40fb
Gabriel Maya Sánchez. <isc_gmayas@hotmail.com>
2024-06-14 17:55:45 -0600

    only commit

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 //import 'bootswatch/dist/darkly/bootstrap.min.css';
 import 'bootswatch/dist/superhero/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-
 import { router } from './router/index.routes'
 router(window.location.hash);
 window.addEventListener('hashchange', () => {
--- src/router/index.routes.js
+++ src/router/index.routes.js
@@ -1,19 +1,26 @@
 
-import { pages}  from "../controllers/index";
+import { pages } from "../controllers/index";
 //
 let content = document.getElementById("content");
 //
 const router = async (router) => {
-   content.innerHTML = "";
-   switch (router) {
-      case "/","#/":
-         return content.appendChild(pages.home());
-      case "#/products":
-         return content.appendChild(pages.products());
-      case "#/post":
-         return content.appendChild(await pages.post());
-      default:
-         return content.appendChild(pages.notFound());
+   try {
+      content.innerHTML = "";
+      switch (router) {
+         case "":
+            return content.appendChild(pages.home());
+         case "#/":
+            return content.appendChild(pages.home());
+         case "#/products":
+            return content.appendChild(pages.products());
+         case "#/post":
+            return content.appendChild(await pages.post());
+         default:
+            return content.appendChild(pages.notFound());
+      }
+   }
+   catch (e) {
+      return content.appendChild(pages.notFound());
    }
 };
 
