748303f2bc17141eed71466c59bd99b3f80215fb
Gabriel Maya Sánchez. <isc_gmayas@hotmail.com>
2024-06-19 15:43:52 -0600

    Only commit

--- src/router/index.routes.js
+++ src/router/index.routes.js
@@ -6,6 +6,21 @@ let content = document.getElementById("content");
 const router = async (router) => {
    try {
       content.innerHTML = "";
+      const regexS = /[?&=\d]/g;
+      const value =  router.match(regexS)
+      console.log('value:', value)
+      var id = 0;
+     if (value !== null){
+         console.log('message.match: ', true)
+         const url = router;
+         const response = `{"${url.replace(/[/?#post]/g,'').trim().slice().replace(/[&]/g,'","').replace(/[=]/g,'":"')}"}` //.split(',');
+         console.log('response:', response)
+         const dataJson = JSON.parse(response)
+         console.log('dataJson:', dataJson.id);
+         id = +dataJson.id
+     }
+     console.log('id:', id);
+     console.log('router:', router);
       switch (router) {
          case "":
             return content.appendChild(pages.home());
@@ -14,6 +29,9 @@ const router = async (router) => {
          case "#/users":
             return content.appendChild(await pages.users());
          case "#/post":
+            return content.appendChild(await pages.post());
+         case `#/post/?id=${id}`:
+            console.log(`#/post/?id=${id}`)
             return content.appendChild(await pages.post());
          default:
             return content.appendChild(pages.notFound());
